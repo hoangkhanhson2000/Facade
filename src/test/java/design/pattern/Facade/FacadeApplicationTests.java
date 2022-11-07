c3c779c916460aee7fa658aa0dee019e77e616b9
hoangkhanhson2000 <hoangkhanhson2000@gmail.com>
2022-11-07 10:20:11 +0700

    Initial commit

---
+++ src/test/java/design/pattern/Facade/FacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package design.pattern.Facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
